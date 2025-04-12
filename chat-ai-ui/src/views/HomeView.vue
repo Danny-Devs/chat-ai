b9a694dd41596bc962ddba926ddf5223e7a9341f
Daniel Ahn <et3rnal.d@gmail.com>
2025-04-11 19:45:32 -0700

    Enhance HomeView and Vite configuration
    - Updated vite.config.ts to include path aliasing for easier imports.
    - Refactored HomeView.vue to improve layout and user input handling.
    - Added reactive properties for name, email, loading, and error states.
    - Enhanced UI with Tailwind CSS for better styling and user experience.

--- chat-ai-ui/src/views/HomeView.vue
+++ chat-ai-ui/src/views/HomeView.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
+import robotImage from '@/assets/robot.png';
+import { ref } from 'vue';
 
+const name = ref('');
+const email = ref('');
+const loading = ref(false);
+const error = ref('');
 </script>
 
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="h-screen flex items-center justify-center bg-gray-900 text-white">
+    <div class="p-12 bg-gray-800 rounded-lg shadow-lg w-full max-w-lg">
+      <img :src="robotImage" alt="Robot" class="mx-auto w-24 h-24 mb-2" />
+      <h1 class="text-2xl font-semibold mb-10 text-center">
+        Welcome to Chat AI
+      </h1>
+
+      <input
+        type="text"
+        class="w-full px-5 py-2 mb-5 bg-gray-700 text-white rounded-lg focus:outline-none placeholder:italic placeholder:text-gray-300 text-center"
+        placeholder="Enter your name"
+        v-model="name"
+      />
+
+      <input
+        type="email"
+        class="w-full px-5 py-2 mb-7 bg-gray-700 text-white rounded-lg focus:outline-none placeholder:italic placeholder:text-gray-300 text-center"
+        placeholder="Enter your email"
+        v-model="email"
+      />
+
+      <button
+        class="w-1/2 p-3 bg-blue-500 rounded-lg mx-auto block"
+        :disabled="loading"
+      >
+        <span v-if="loading">Loading...</span>
+        <span v-else>Login</span>
+      </button>
+    </div>
   </div>
 </template>
